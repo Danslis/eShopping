4a0455b6432b3c688255724208fbb57a628bf299
Dart <Danslis@yandex.ru>
2024-03-10 13:20:46 +0300

    Интерфейсы для получения данных

---
+++ Services/Catalog/Catalog.Core/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProducts();
+    Task<Product> GetProduct(string id);
+    Task<IEnumerable<Product>> GetProducntByName(string name);
+    Task<IEnumerable<Product>> GetProducntByBrand(string brand);
+    Task<Product> CreateProduct(Product product);
+    Task<Product> UpdateProduct(Product product);
+    Task<Product> DeleteProduct(string id);
+
+}
