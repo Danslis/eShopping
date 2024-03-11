075418a3bba56622a34d224f3bd44e6122ca5bcc
Dart <Danslis@yandex.ru>
2024-03-11 20:21:46 +0300

    Startup Registration

--- Services/Catalog/Catalog.API/Startup.cs
+++ Services/Catalog/Catalog.API/Startup.cs
@@ -1,4 +1,13 @@
-﻿namespace Catalog.API;
+﻿using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Catalog.API;
 
 public class Startup
 {
@@ -12,6 +21,20 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {       
         services.AddControllers();       
+        services.AddApiVersioning();
+        services.AddHealthChecks()
+             .AddMongoDb(Configuration["DatabaseSettings:ConnectionString"], "Catalog  Mongo Db Health Check",
+                 HealthStatus.Degraded);
+
+        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" }); });
+
+        //DI
+        services.AddAutoMapper(typeof(Startup));
+        services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly);        
+        services.AddScoped<ICatalogContext, CatalogContext>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IBrandRepository, ProductRepository>();
+        services.AddScoped<ITypesRepository, ProductRepository>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
