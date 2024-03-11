8d6642303846745492b1c3c0b3f6dfc59bbafec3
Dart <Danslis@yandex.ru>
2024-03-11 20:00:44 +0300

    Delete Product Handler

--- Services/Catalog/Catalog.Application/Catalog.Application/Mappers/ProductMappingProfile.cs
+++ Services/Catalog/Catalog.Application/Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -1,13 +1,18 @@
 ﻿using AutoMapper;
+using Catalog.Application.Commands;
 using Catalog.Application.Responses;
 using Catalog.Core.Entities;
+using Catalog.Core.Specs;
 
 namespace Catalog.Application.Mappers;
 
 public class ProductMappingProfile : Profile
 {
     public ProductMappingProfile()
-    {     
-        CreateMap<ProductBrand, BrandResponse>().ReverseMap();      
+    {
+        CreateMap<Product, ProductResponse>().ReverseMap();
+        CreateMap<Product, CreateProductCommand>().ReverseMap();
+        CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+        CreateMap<ProductType, TypesResponse>().ReverseMap();      
     }
 }
