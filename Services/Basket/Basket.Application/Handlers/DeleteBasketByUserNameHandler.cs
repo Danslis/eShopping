8a7d93cbce476bc586e3eadea003ce90d420c063
Dart <Danslis@yandex.ru>
2024-03-16 20:40:09 +0300

    Adding Nuget Packages in Basket API Project

--- Services/Basket/Basket.Application/Handlers/DeleteBasketByUserNameHandler.cs
+++ Services/Basket/Basket.Application/Handlers/DeleteBasketByUserNameHandler.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Basket.Application.Queries;
+using Basket.Core.Repositories;
+using MediatR;
 
-namespace Basket.Application.Handlers
+namespace Basket.Application.Handlers;
+
+public class DeleteBasketByUserNameHandler : IRequestHandler<DeleteBasketByUserNameQuery>
 {
-    internal class DeleteBasketByUserNameHandler
+    private readonly IBasketRepository _basketRepository;
+
+    public DeleteBasketByUserNameHandler(IBasketRepository basketRepository)
     {
+        _basketRepository = basketRepository;
+    }
+    public async Task<Unit> Handle(DeleteBasketByUserNameQuery request, CancellationToken cancellationToken)
+    {
+        await _basketRepository.DeleteBasket(request.UserName);
+        return Unit.Value;
     }
 }
--- Services/Basket/Basket.Application/Queries/DeleteBasketByUserNameQuery.cs
+++ Services/Basket/Basket.Application/Queries/DeleteBasketByUserNameQuery.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
-namespace Basket.Application.Queries
+namespace Basket.Application.Queries;
+
+public class DeleteBasketByUserNameQuery : IRequest
 {
-    internal class DeleteBasketByUserNameQuery
+    public string UserName { get; set; }
+
+    public DeleteBasketByUserNameQuery(string userName)
     {
+        UserName = userName;
     }
 }
