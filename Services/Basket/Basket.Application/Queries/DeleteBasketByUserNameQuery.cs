7510f3892e571dee74f830c87d10d16b6da7a02a
Dart <Danslis@yandex.ru>
2024-03-16 20:36:48 +0300

    Delete Basket By UserName

---
+++ Services/Basket/Basket.Application/Queries/DeleteBasketByUserNameQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Application.Queries
+{
+    internal class DeleteBasketByUserNameQuery
+    {
+    }
+}
