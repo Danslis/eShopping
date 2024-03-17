e19ac0ce317907d4ef94c69b1754b983a9d48930
Dart <Danslis@yandex.ru>
2024-03-17 14:47:10 +0300

    Get Discount Query and Handler

---
+++ Services/Discount/Discount.Application/Handlers/GetDiscountQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.Application.Handlers
+{
+    internal class GetDiscountQueryHandler
+    {
+    }
+}
