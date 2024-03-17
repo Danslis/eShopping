038d441ef94fe7f6d0651cc16462ba78fcf14d35
Dart <Danslis@yandex.ru>
2024-03-17 15:06:55 +0300

    Startup File Creation

--- Services/Discount/Discount.Application/Commands/DeleteDiscountCommand.cs
+++ Services/Discount/Discount.Application/Commands/DeleteDiscountCommand.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
-namespace Discount.Application.Commands
+namespace Discount.Application.Commands;
+
+public class DeleteDiscountCommand : IRequest<bool>
 {
-    internal class DeleteDiscountCommand
+    public string ProductName { get; set; }
+
+    public DeleteDiscountCommand(string productName)
     {
+        ProductName = productName;
     }
 }
--- Services/Discount/Discount.Application/Handlers/DeleteDiscountCommandHandler.cs
+++ Services/Discount/Discount.Application/Handlers/DeleteDiscountCommandHandler.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discount.Application.Commands;
+using Discount.Core.Repositories;
+using MediatR;
 
-namespace Discount.Application.Handlers
+namespace Discount.Application.Handlers;
+
+public class DeleteDiscountCommandHandler : IRequestHandler<DeleteDiscountCommand, bool>
 {
-    internal class DeleteDiscountCommandHandler
+    private readonly IDiscountRepository _discountRepository;
+
+    public DeleteDiscountCommandHandler(IDiscountRepository discountRepository)
     {
+        _discountRepository = discountRepository;
+    }
+    public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+    {
+        var deleted = await _discountRepository.DeleteDiscount(request.ProductName);
+        return deleted;
     }
 }
