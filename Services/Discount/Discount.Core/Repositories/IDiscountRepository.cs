9ac084ca61b803585f5c8a9be1b0a0edd147b3e1
Dart <Danslis@yandex.ru>
2024-03-17 14:02:49 +0300

    Repository Implementation

--- Services/Discount/Discount.Core/Repositories/IDiscountRepository.cs
+++ Services/Discount/Discount.Core/Repositories/IDiscountRepository.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discount.Core.Entities;
 
-namespace Discount.Core.Repositories
+namespace Discount.Core.Repositories;
+
+public interface IDiscountRepository
 {
-    internal interface IDiscountRepository
-    {
-    }
+    Task<Coupon> GetDiscount(string productName);
+    Task<bool> CreateDiscount(Coupon coupon);
+    Task<bool> UpdateDiscount(Coupon coupon);
+    Task<bool> DeleteDiscount(string productName);
 }
